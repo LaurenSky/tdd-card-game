c1feb1df238da6a8f2ba988e57094c355f1b875e
LaurenSky <ljherrer@gmail.com>
2016-08-29 15:25:35 -0700

    used variable and defined appropriate card values and suits

--- card.rb
+++ card.rb
@@ -1,7 +1,25 @@
 class Card
   attr_reader :suit, :value
 
+  SUITS = [
+    :hearts,
+    :diamonds,
+    :clubs,
+    :spades
+  ]
+
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
   def initialize (suit, val)
+
+    if !SUITS.include?(suit)
+      raise ArgumentError.new("Invalid Suit")
+    end
+
+    if !VALUES.include?(val)
+      raise ArgumentError.new("Invalid Value")
+    end
+
     @suit = suit
     @value = val
   end
--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -6,37 +6,40 @@ require_relative '../card'  #../   alows you to look one folder up from where yo
 #describe is a minitest keyword
 describe Card do
   describe "#initialize" do
+    #card = Card.new("suit", 1)
+    let(:card) { Card.new(:hearts, "K") }
+    #let allows you to set up a variable and it kind of just hangs out until that variable is needed. Allows us to continue running tests
 
     it "can create a new instance of Card" do
-      Card.new("suit", 1).must_be_instance_of(Card)
+      card.must_be_instance_of(Card)
     end
 
     it "should have a suit property" do
-      Card.new("suit", 1).must_respond_to(:suit)
-      #or
-      #c = Card.new("suit", 1)
-      #c.must_respond_to(:suit)
+      card.must_respond_to(:suit)
     end
 
     it "should have a value property" do
-      Card.new("suit", 1).must_respond_to(:value)
-      #or
-      #c = Card.new("suit", 1)
-      #c.must_respond_to(:value)
+      card.must_respond_to(:value)
     end
 
     it "suit should have the same value that we set" do
-      Card.new("suit", 1).suit.must_equal("suit")
-      # OR
-      # c = Card.new("suit", 1)
-      # c.suit.must_equal("suit")
+      card.suit.must_equal(:hearts)
     end
 
     it "value should have the same value that we set" do
-      Card.new("suit", 1).value.must_equal(1)
-      # OR
-      # c = Card.new("suit", 1)
-      # c.value.must_equal(1)
+      card.value.must_equal("K")
+    end
+
+    it "should throw an error when an invalid suit is supplied" do
+      proc { Card.new("suit", 2) }.must_raise(ArgumentError)
+    end
+
+    it "should be in the valid list of suits" do
+      Card::SUITS.must_include(card.suit)
+    end
+
+    it "should throw an error when an invalid value is supplied" do
+      proc { Card.new(:spades, 1) }.must_raise(ArgumentError)
     end
 
   end

a72d112cba8d3906020fb14601a4349d8c148473
LaurenSky <ljherrer@gmail.com>
2016-08-30 15:07:39 -0700

    added deck spec and deck methods

---
+++ specs/deck_spec.rb
@@ -0,0 +1,38 @@
+require_relative 'spec_helper'
+require_relative '../deck'  #../   alows you to look one folder up from where you are
+
+# type this in tereminal to see test clearly in terminal:   clear; rake
+
+#describe is a minitest keyword
+describe Deck do
+  describe "#initialize" do
+    it "can create a new Deck instance" do
+      Deck.new([]).must_be_instance_of(Deck)
+    end
+
+    it "should raise an error when a non-array is passed in" do
+      proc { Deck.new({}) }.must_raise(ArgumentError)
+    end
+  end
+
+  describe "create_standard" do
+    it "will return a Deck object" do
+      Deck.create_standard.must_be_instance_of(Deck)
+    end
+
+    it "will return a Deck with 52 Cards" do
+      std_deck = Deck.create_standard
+      std_deck.length.must_equal(52)
+    end
+  end
+
+  describe "#shuffle" do
+    it "should return a different array than it started with" do
+      deck = Deck.create_standard   # Kari doesn't love this, but it makes our test faster
+      original_cards = deck.cards.clone
+      deck.shuffle.wont_equal(original_cards)
+    end
+  end
+
+
+end
